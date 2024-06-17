4692900dca950b69f86c8970640b00282477a1e8
Yannick <Yannick.Ackermann@nexus-ag.de>
2024-06-17 11:21:31 +0200

    My commit in Eclipse!

--- myJavaTest.java
+++ myJavaTest.java
@@ -2,4 +2,9 @@ public class myJavaTest {
     public static void main(String[] args) {
         System.out.println("hello world!");
     }
+    
+    //This was added in Eclipse
+    public void output(){
+    	System.out.println("hello earth!");
+    }
 }
